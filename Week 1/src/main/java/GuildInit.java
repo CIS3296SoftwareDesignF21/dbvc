4347ec25cf67736f46a1ed78add3e6bae51d8fc7
Rachel Lazzaro <r.lazz@outlook.com>
2021-11-09 08:53:08 -0500

    created GuildInit class

---
+++ Week 1/src/main/java/GuildInit.java
@@ -0,0 +1,29 @@
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+import java.util.List;
+
+public class GuildInit extends ListenerAdapter {
+    //private final long userId;
+
+   // public GuildInit(User user){
+    //    this.userId = user.getIdLong();
+    //}
+
+    @Override
+    public void onGuildMemberJoin(GuildMemberJoinEvent event){
+        //JDA api = event.getJDA();
+        String user = event.getMember().getAsMention();
+        System.out.println("user just landed: " + user);
+
+        List<TextChannel> channels = event.getGuild().getTextChannelsByName("general", true);
+
+        for(TextChannel channel : channels){
+            channel.sendMessage("New member joined: " + user).queue();
+        }
+
+        event.getMember().getUser().openPrivateChannel().flatMap(channel ->
+                channel.sendMessage("Welcome to the server, " + event.getMember().getAsMention())).queue();
+    }
+}
