382c8b123314d0c12eadedda4c4995a1f8b62be6
Lin Li Oechsle <tui20361@temple.edu>
2021-11-08 22:15:21 -0500

    handled emoji comparisons

--- Week 1/src/main/java/Main.java
+++ Week 1/src/main/java/Main.java
@@ -2,6 +2,9 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
+import net.dv8tion.jda.api.events.message.react.MessageReactionRemoveEvent;
+
 import javax.security.auth.login.LoginException;
 
 public class Main extends ListenerAdapter {
@@ -29,6 +32,7 @@ public class Main extends ListenerAdapter {
             event.getChannel().sendMessage("Hello Rachel!").queue();
         }
     }
+
 
 
 }
