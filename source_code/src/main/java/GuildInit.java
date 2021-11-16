8ce94ada249ab9c740bde909c60ff74b4184b97c
Rachel Lazzaro <r.lazz@outlook.com>
2021-11-15 22:43:06 -0500

    added initialization function with !start command
    - added !roles command
    - added server creation of roles and channels

--- source_code/src/main/java/GuildInit.java
+++ source_code/src/main/java/GuildInit.java
@@ -1,69 +1,57 @@
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Role;
-import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
-import java.awt.*;
-import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class GuildInit extends ListenerAdapter {
-    String welcome = "Welcome to the server! I hope that you are ready to have some fun - before you begin your journey," +
+    String welcome = "Welcome to the server! I hope that you are ready to have some fun - before you begin your journey, " +
             "please know that you will have limited access to the server as a new member for the next 2 hours. After that, you " +
-            "will have access to the server based on your background and history.";
+            "will have access to the server based on your background and history. Head over to the 'welcome' text channel to begin!";
 
     class removeNewRole extends TimerTask{
         // constructor
         GuildMemberJoinEvent event;
-        Role role;
+        Role newMember;
         removeNewRole(GuildMemberJoinEvent e){
             this.event = e;
-            this.role = e.getGuild().getRolesByName("New Member", true).get(0);
+            this.newMember = e.getGuild().getRolesByName("New Member", true).get(0);
         }
 
         public void run(){
-            event.getGuild().removeRoleFromMember(event.getMember(), role).queue();
-            event.getMember().getUser().openPrivateChannel().flatMap(channel ->
-                    channel.sendMessage("Removing restriction - have fun :)")).queue();
+            event.getGuild().removeRoleFromMember(event.getMember(), newMember).queue();
+
+            if(event.getMember().getRoles().get(0) == null){
+                // if they have no roles assigned currently, they have already been authorized.
+                event.getMember().getUser().openPrivateChannel().flatMap(channel ->
+                        channel.sendMessage("Your new member restriction has been removed - have fun on the server :)")).queue();
+            } else {
+                event.getMember().getUser().openPrivateChannel().flatMap(channel ->
+                        channel.sendMessage("Your new member restriction has been removed, but you still haven't agreed to server rules. " +
+                                "Please follow the instructions in the authorization channel to get access to the server!")).queue();
+            }
         }
     }
 
     @Override
     public void onGuildMemberJoin(GuildMemberJoinEvent event){
         //JDA api = event.getJDA();
-
-        if(event.getGuild().getRolesByName("New Member", true).isEmpty()){
-            event.getGuild().createRole()
-                    .setName("New Member")
-                    .setColor(Color.PINK)
-                    .setPermissions(0L)
-                    .setHoisted(true)
-                    .complete();
-        }
+        Member member = event.getMember();
+        Guild guild = event.getGuild();
         Timer timer = new Timer();
         // current delay is in seconds
         int delay = 60;
         timer.schedule(new removeNewRole(event), delay*1000);
 
-        Role newMember = event.getGuild().getRolesByName("New Member", true).get(0);
-       //System.out.println("role: " + newMember.getName());
-        event.getGuild().addRoleToMember(event.getMember(), newMember).queue();
+        Role newMember = guild.getRolesByName("New Member", true).get(0);
+        Role unauth = guild.getRolesByName("unauth", true).get(0);
+        guild.addRoleToMember(member, newMember).queue();
+        guild.addRoleToMember(member, unauth).queue();
 
-        String user = event.getMember().getAsMention();
-        // ADD FUNCTIONALITY TO ADD USER TO NEW MEMBER ROLE
-
-        List<TextChannel> channels = event.getGuild().getTextChannelsByName("general", true);
-        for(TextChannel channel : channels){
-            channel.sendMessage("Let's all welcome " + user + "!").queue();
-        }
-
-        List<Role> roles = event.getGuild().getRoles();
         event.getMember().getUser().openPrivateChannel().flatMap(channel ->
                 channel.sendMessage(welcome)).queue();
-        for(Role role : roles){
-            event.getMember().getUser().openPrivateChannel().flatMap(channel ->
-                    channel.sendMessage(role.getName())).queue();
-        }
     }
 }
--- source_code/src/main/java/RoleAssignment.java
+++ source_code/src/main/java/RoleAssignment.java
@@ -1,5 +1,5 @@
 import net.dv8tion.jda.api.JDA;
-import net.dv8tion.jda.api.entities.MessageReaction;
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
 import net.dv8tion.jda.api.events.message.react.MessageReactionRemoveEvent;
@@ -8,7 +8,7 @@ import net.dv8tion.jda.api.events.message.react.MessageReactionRemoveEvent;
 to initialized the role values, or find a way for the user to set up the roles themselves within the server instead
 of editing code
  */
-// make sure the reactions only apply to messages in the roles channel
+// make sure the reactions only apply to messages in the roles channel and the welcome channel
 public class RoleAssignment extends ListenerAdapter {
     private static JDA jda;
     private long babyWitch = 907370752519585823L;
@@ -48,4 +48,204 @@ public class RoleAssignment extends ListenerAdapter {
             }
         }
     }
+
+    // when a user reacts to a message in the roles channel, they are added to the associated role
+    /**
+    public void onMessageReactionAdd (MessageReactionAddEvent event){
+        Guild g = event.getGuild();
+        String channel = event.getChannel().toString();
+        String welcomeID = g.getTextChannelsByName("welcome", true).get(0).getId();
+        String rolesID = g.getTextChannelsByName("roles", true).get(0).getId();
+        String reaction = event.getReactionEmote().toString();
+
+        if(channel.equals(welcomeID)){
+            if(reaction.equals("RE:U+270d")){
+                System.out.println("Removing role based on reaction: " + reaction);
+                g.removeRoleFromMember(event.getUserId(), g.getRolesByName("unauth", true).get(0)).queue();
+            }
+        } else if(channel.equals(rolesID)){
+            System.out.println("channel id: " + event.getTextChannel());
+            System.out.println(event.getUser().getName() + " just reacted to a message");
+
+            switch (reaction) {
+                case "RE:U+1f98b":  // baby witch
+                    g.addRoleToMember(event.getUserId(), g.getRolesByName("baby witch", true).get(0)).queue();
+                    System.out.println("Added user to baby witch role");
+                    break;
+                case "RE:U+1f337":  // guest
+                    g.addRoleToMember(event.getUserId(), g.getRolesByName("guest", true).get(0)).queue();
+                    System.out.println("Added user to guest role");
+                    break;
+                case "RE:U+1f52e":  // sage witch
+                    g.addRoleToMember(event.getUserId(), g.getRolesByName("sage witch", true).get(0)).queue();
+                    System.out.println("Added user to sage witch role");
+                    break;
+                case "RE:U+1f7e9":  // she/her
+                    g.addRoleToMember(event.getUserId(), g.getRolesByName("she/her", true).get(0)).queue();
+                    System.out.println("Added user to she/her role");
+                    break;
+                case "RE:U+1f7e6":  // he/him
+                    g.addRoleToMember(event.getUserId(), g.getRolesByName("he/him", true).get(0)).queue();
+                    System.out.println("Added user to he/him role");
+                    break;
+                case "RE:U+1f7ea":  // they/them
+                    g.addRoleToMember(event.getUserId(), g.getRolesByName("they/them", true).get(0)).queue();
+                    System.out.println("Added user to they/them role");
+                    break;
+                case "RE:U+2648":  // aries
+                    g.addRoleToMember(event.getUserId(), g.getRolesByName("aries", true).get(0)).queue();
+                    System.out.println("Added user to aries role");
+                    break;
+                case "RE:U+2649":  // taurus
+                    g.addRoleToMember(event.getUserId(), g.getRolesByName("taurus", true).get(0)).queue();
+                    System.out.println("Added user to taurus role");
+                    break;
+                case "RE:U+264A":  // gemini
+                    g.addRoleToMember(event.getUserId(), g.getRolesByName("gemini", true).get(0)).queue();
+                    System.out.println("Added user to gemini role");
+                    break;
+                case "RE:U+264B":  // cancer
+                    g.addRoleToMember(event.getUserId(), g.getRolesByName("cancer", true).get(0)).queue();
+                    System.out.println("Added user to cancer role");
+                    break;
+                case "RE:U+264C":  // leo
+                    g.addRoleToMember(event.getUserId(), g.getRolesByName("leo", true).get(0)).queue();
+                    System.out.println("Added user to leo role");
+                    break;
+                case "RE:U+264D":  // virgo
+                    g.addRoleToMember(event.getUserId(), g.getRolesByName("virgo", true).get(0)).queue();
+                    System.out.println("Added user to virgo role");
+                    break;
+                case "RE:U+264E":  // libra
+                    g.addRoleToMember(event.getUserId(), g.getRolesByName("libra", true).get(0)).queue();
+                    System.out.println("Added user to libra role");
+                    break;
+                case "RE:U+264F":  // scorpio
+                    g.addRoleToMember(event.getUserId(), g.getRolesByName("scorpio", true).get(0)).queue();
+                    System.out.println("Added user to scorpio role");
+                    break;
+                case "RE:U+2650":  // sagittarius
+                    g.addRoleToMember(event.getUserId(), g.getRolesByName("sagittarius", true).get(0)).queue();
+                    System.out.println("Added user to sagittarius role");
+                    break;
+                case "RE:U+2651":  // capricorn
+                    g.addRoleToMember(event.getUserId(), g.getRolesByName("capricorn", true).get(0)).queue();
+                    System.out.println("Added user to capricorn role");
+                    break;
+                case "RE:U+2652":  // aquarius
+                    g.addRoleToMember(event.getUserId(), g.getRolesByName("aquarius", true).get(0)).queue();
+                    System.out.println("Added user to aquarius role");
+                    break;
+                case "RE:U+2653":  // pisces
+                    g.addRoleToMember(event.getUserId(), g.getRolesByName("pisces", true).get(0)).queue();
+                    System.out.println("Added user to pisces role");
+                    break;
+                default:
+                    break;
+            }
+            System.out.println("Added role based on reaction: " + reaction);
+        }
+    }
+
+    // if a user removes their reaction from a message in the roles channel, they will be removed from the associated role
+    public void onMessageReactionRemove (MessageReactionRemoveEvent event) {
+        Guild g = event.getGuild();
+        String channel = event.getChannel().toString();
+        String welcomeID = g.getTextChannelsByName("welcome", true).get(0).getId();
+        String rolesID = g.getTextChannelsByName("roles", true).get(0).getId();
+        String reaction = event.getReactionEmote().toString();
+
+        if(channel.equals(welcomeID)){
+            if(reaction.equals("RE:U+270d")){
+                System.out.println("Adding role based on reaction: " + reaction);
+                g.addRoleToMember(event.getUserId(), g.getRolesByName("unauth", true).get(0)).queue();
+
+                event.getMember().getUser().openPrivateChannel().flatMap(privateMessage ->
+                        privateMessage.sendMessage("You have removed acknowledgement to server rules, and are no longer authorized. "+
+                                "Please react once more to gain access back to the server.")).queue();
+            }
+        } else if(channel.equals(rolesID)){
+            System.out.println("channel id: " + event.getTextChannel());
+            System.out.println(event.getUser().getName() + " just reacted to a message");
+
+            switch (reaction) {
+                case "RE:U+1f98b":  // baby witch
+                    g.removeRoleFromMember(event.getUserId(), g.getRolesByName("baby witch", true).get(0)).queue();
+                    System.out.println("Removed user to baby witch role");
+                    break;
+                case "RE:U+1f337":  // guest
+                    g.removeRoleFromMember(event.getUserId(), g.getRolesByName("guest", true).get(0)).queue();
+                    System.out.println("Removed user to guest role");
+                    break;
+                case "RE:U+1f52e":  // sage witch
+                    g.removeRoleFromMember(event.getUserId(), g.getRolesByName("sage witch", true).get(0)).queue();
+                    System.out.println("Removed user to sage witch role");
+                    break;
+                case "RE:U+1f7e9":  // she/her
+                    g.removeRoleFromMember(event.getUserId(), g.getRolesByName("she/her", true).get(0)).queue();
+                    System.out.println("Removed user to she/her role");
+                    break;
+                case "RE:U+1f7e6":  // he/him
+                    g.removeRoleFromMember(event.getUserId(), g.getRolesByName("he/him", true).get(0)).queue();
+                    System.out.println("Removed user to he/him role");
+                    break;
+                case "RE:U+1f7ea":  // they/them
+                    g.removeRoleFromMember(event.getUserId(), g.getRolesByName("they/them", true).get(0)).queue();
+                    System.out.println("Removed user to they/them role");
+                    break;
+                case "RE:U+2648":  // aries
+                    g.removeRoleFromMember(event.getUserId(), g.getRolesByName("aries", true).get(0)).queue();
+                    System.out.println("Removed user to aries role");
+                    break;
+                case "RE:U+2649":  // taurus
+                    g.removeRoleFromMember(event.getUserId(), g.getRolesByName("taurus", true).get(0)).queue();
+                    System.out.println("Removed user to taurus role");
+                    break;
+                case "RE:U+264A":  // gemini
+                    g.removeRoleFromMember(event.getUserId(), g.getRolesByName("gemini", true).get(0)).queue();
+                    System.out.println("Removed user to gemini role");
+                    break;
+                case "RE:U+264B":  // cancer
+                    g.removeRoleFromMember(event.getUserId(), g.getRolesByName("cancer", true).get(0)).queue();
+                    System.out.println("Removed user to cancer role");
+                    break;
+                case "RE:U+264C":  // leo
+                    g.removeRoleFromMember(event.getUserId(), g.getRolesByName("leo", true).get(0)).queue();
+                    System.out.println("Removed user to leo role");
+                    break;
+                case "RE:U+264D":  // virgo
+                    g.removeRoleFromMember(event.getUserId(), g.getRolesByName("virgo", true).get(0)).queue();
+                    System.out.println("Removed user to virgo role");
+                    break;
+                case "RE:U+264E":  // libra
+                    g.removeRoleFromMember(event.getUserId(), g.getRolesByName("libra", true).get(0)).queue();
+                    System.out.println("Removed user to libra role");
+                    break;
+                case "RE:U+264F":  // scorpio
+                    g.removeRoleFromMember(event.getUserId(), g.getRolesByName("scorpio", true).get(0)).queue();
+                    System.out.println("Removed user to scorpio role");
+                    break;
+                case "RE:U+2650":  // sagittarius
+                    g.removeRoleFromMember(event.getUserId(), g.getRolesByName("sagittarius", true).get(0)).queue();
+                    System.out.println("Removed user to sagittarius role");
+                    break;
+                case "RE:U+2651":  // capricorn
+                    g.removeRoleFromMember(event.getUserId(), g.getRolesByName("capricorn", true).get(0)).queue();
+                    System.out.println("Removed user to capricorn role");
+                    break;
+                case "RE:U+2652":  // aquarius
+                    g.removeRoleFromMember(event.getUserId(), g.getRolesByName("aquarius", true).get(0)).queue();
+                    System.out.println("Removed user to aquarius role");
+                    break;
+                case "RE:U+2653":  // pisces
+                    g.removeRoleFromMember(event.getUserId(), g.getRolesByName("pisces", true).get(0)).queue();
+                    System.out.println("Removed user to pisces role");
+                    break;
+                default:
+                    break;
+            }
+            System.out.println("Added role based on reaction: " + reaction);
+        }
+    }
+     **/
 }
